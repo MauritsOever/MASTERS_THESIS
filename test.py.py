7715e3d3eb7463caa66f38b4137be4d68f5ccf13
MauritsOever <71381155+MauritsOever@users.noreply.github.com>
2022-03-15 11:50:05 +0100

    Update test.py.py

--- test.py.py
+++ test.py.py
@@ -1,17 +1,33 @@
 # -*- coding: utf-8 -*-
 """
 Trying to implement some VAEs over here...
+Useless:
 https://i-systems.github.io/teaching/ML/iNotes/15_Autoencoder.html#3.-Autoencoder-with-Scikit-Learn
+
+
+Useful (maybe):
+https://keras.io/examples/generative/vae/ regular VAE
+https://github.com/AntixK/PyTorch-VAE
+https://blog.paperspace.com/how-to-build-variational-autoencoder-keras/
+https://colab.research.google.com/github/tvhahn/Manufacturing-Data-Science-with-Python/blob/master/Metal%20Machining/1.B_building-vae.ipynb
+https://towardsdatascience.com/teaching-a-variational-autoencoder-vae-to-draw-mnist-characters-978675c95776
+https://www.youtube.com/watch?v=A6mdOEPGM1E regular VAE
+https://github.com/fpcasale/GPPVAE gaussian
+https://jmetzen.github.io/2015-11-27/vae.html not entirely gaussian
+https://jaketae.github.io/study/vae/ gaussian diagonal
+https://tiao.io/post/tutorial-on-variational-autoencoders-with-a-concise-keras-implementation/ but generalise to complicated likelihoods 
+https://towardsdatascience.com/on-distribution-of-zs-in-vae-bb9a05b530c6 on the KL divergence
+
+
+
+
 
 Created on Fri Mar  4 11:16:29 2022
 
 @author: MauritsOever
 """
 
-from sknn import ae
-from sklearn.neural_network import MLPRegressor
-import matplotlib.pyplot as plt
-import numpy as np
+
 
 def Yahoo(list_of_ticks, startdate, enddate, retsorclose = 'rets'):
     '''
@@ -40,69 +56,33 @@ def Yahoo(list_of_ticks, startdate, enddate, retsorclose = 'rets'):
     else:
         return dfclose
     
+def GenerateNormalData(list_of_tuples, n):
+    """
+    Parameters
+    ----------
+    list_of_tuples : the tuples contain the mean and var of the variables, the length
+                     the list determines the amount of variables
+    n              : int, amount of observations
+
+    Returns
+    -------
+    an m by n array of uncorrelated normal data (diagonal covar matrix)
+
+    """
+    import numpy as np
+    array = np.empty((n,len(list_of_tuples)))
     
-# data definition
-data  = Yahoo(['AAPL', 'ZION', 'NVDA', 'EBAY', 'MSFT', 'IPG'], startdate = '2000-01-01', enddate='2022-01-01').iloc[1:, :]
-data = data.ffill()
-
-# Shape of input and latent variable
-
-n_input = 28*28
-
-# Encoder structure
-n_encoder1 = 6
-n_encoder2 = 4
-
-n_latent = 2
-
-# Decoder structure
-n_decoder2 = 4
-n_decoder1 = 6
-
-
-reg = MLPRegressor(hidden_layer_sizes = (n_encoder1, n_encoder2, n_latent, n_decoder2, n_decoder1), 
-                   activation = 'tanh', 
-                   solver = 'adam', 
-                   learning_rate_init = 0.0001, 
-                   max_iter = 10000000, 
-                   tol = 0.0000001, 
-                   verbose = True)
-
-reg.fit(data, data)
-
-
-# getting some info on the latent data
-def encoder(data):
-    data = np.asmatrix(data)
+    for variable in range(len(list_of_tuples)):
+        array[:,variable] = np.random.normal(list_of_tuples[variable][0], list_of_tuples[variable][1], n)
+        
+    return array
     
-    encoder1 = data*reg.coefs_[0] + reg.intercepts_[0]
-    encoder1 = (np.exp(encoder1) - np.exp(-encoder1))/(np.exp(encoder1) + np.exp(-encoder1))
     
-    encoder2 = encoder1*reg.coefs_[1] + reg.intercepts_[1]
-    encoder2 = (np.exp(encoder2) - np.exp(-encoder2))/(np.exp(encoder2) + np.exp(-encoder2))
+n = 100000
+list_of_tuples = [(0,1),(1,0.01),(40,12),(0,1)]
+test = GenerateNormalData(list_of_tuples, n)
     
-    latent = encoder2*reg.coefs_[2] + reg.intercepts_[2]
-    latent = (np.exp(latent) - np.exp(-latent))/(np.exp(latent) + np.exp(-latent))
     
-    return np.asarray(latent)
-
-test_latent = encoder(data)
-
-plt.figure(figsize = (10,10))
-#plt.scatter(test_latent[np.argmax(data, axis = 1) == 1,0], test_latent[np.argmax(data, axis = 1) == 1,1], label = '1')
-
-plt.scatter(test_latent[:,0], test_latent[:,1])
-plt.title('Latent Space', fontsize=15)
-plt.xlabel('Z1', fontsize=15)
-plt.ylabel('Z2', fontsize=15)
-plt.legend(fontsize = 15)
-plt.axis('equal')
-plt.show()
-
-
-
-
-
-
-# data generation
-
+    
+    
+    
