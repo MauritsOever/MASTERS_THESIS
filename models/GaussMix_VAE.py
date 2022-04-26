c7ac4e10466a79253e9543cd13eedbdeded8e408
MauritsOever <mauritsvandenoever@hotmail.com>
2022-04-26 17:13:16 +0200

    Update GaussMix_VAE.py

--- models/GaussMix_VAE.py
+++ models/GaussMix_VAE.py
@@ -3,7 +3,21 @@
 Created on Wed Apr 20 16:04:34 2022
 
 @author: gebruiker
+
+
+https://github.com/mazrk7/gmvae
+https://github.com/jariasf/GMVAE
+https://arxiv.org/pdf/1906.09333.pdf
+https://stats.stackexchange.com/questions/350921/variational-autoencoder-with-gaussian-mixture-model
+https://bzong.github.io/doc/iclr18-dagmm.pdf
+http://lijiaying.github.io/papers/iccv19.pdf
+
 """
+
+
+
+
+
 # imports
 from collections import OrderedDict
 import numpy as np
@@ -15,3 +29,6 @@ class GaussMixVAE: #(nn.Module):
     
     def __init__(self):
         print('Im not done yet!')
+
+
+
